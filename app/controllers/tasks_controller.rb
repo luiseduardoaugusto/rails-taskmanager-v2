b37cb21a558dbf499c7fe5b2cbd854bd17ae34bc
luis augusto <luis.augusto.u@gmail.com>
2023-09-29 14:05:02 -0500

    agregue bootstrap y otras cosas

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
-  before_action
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -13,7 +13,22 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task
+    task = Task.new(task_params)
+    task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
@@ -23,7 +38,7 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:completed)
+    params.require(:task).permit(:title, :details, :completed)
   end
 
 end
